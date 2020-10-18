13d6c092a0d42a03b75696353e0d3d025c066be7
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-10-18 12:40:29 +0800

    Fixed heading issue when content is not defined

---
+++ src/components/heading.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Typography } from 'antd';
+import _ from 'lodash';
+const { Title } = Typography;
+const crypto = require('crypto');
+const hash = (text) => crypto.createHash('sha1').update(text).digest('hex');
+
+const heading = (level) => {
+  return (props) => {
+    const text = _.get(props, 'children[0]', 'N/A')
+    return <Title id={hash(text)} level={level}>{text}</Title>
+  }
+};
+
+const headings = {};
+
+for (const i in [1, 2, 3, 4, 5]) {
+  headings['Heading' + i] = heading(i)
+}
+
+export const { Heading1, Heading2, Heading3, Heading4, Heading5 } = headings;
