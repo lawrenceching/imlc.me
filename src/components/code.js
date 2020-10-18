3ddd39183faf4b440727e78aa1aad045d9a78ad3
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-10-18 11:43:49 +0800

    Do not show copy button if code has less than 6 lines

--- src/components/code.js
+++ src/components/code.js
@@ -1,7 +1,6 @@
 import React from "react"
 import _ from 'lodash';
-import { CopyBlock, github } from "react-code-blocks";
-
+import {CopyBlock, CodeBlock, github} from "react-code-blocks";
 
 const Code = (props) => {
   const code = props.children[0];
@@ -9,21 +8,37 @@ const Code = (props) => {
 
   // bash / shell is not supported due to below issue.
   // https://github.com/rajinwonderland/react-code-blocks/issues/19
-  if(['shell', 'bash', 'sh', 'ksh', 'zsh'].includes(lang)) {
-    console.error(`Unsupported language: ${lang} for code block, fallback to default language "text"`);
+  if (['shell', 'bash', 'sh', 'ksh', 'zsh'].includes(lang)) {
+    console.error(
+        `Unsupported language: ${lang} for code block, fallback to default language "text"`);
     lang = 'text';
   }
 
-  return (
-      <CopyBlock
-          text={code}
-          language={lang}
-          showLineNumbers={true}
-          theme={github}
-          wrapLines
-          codeBlock
-      />
-  );
+  const lines = code.split(/\r\n|\r|\n/).length;
+
+  if (lines <= 5) {
+    return (
+        <CodeBlock
+            text={code}
+            language={lang}
+            showLineNumbers={lines > 5}
+            theme={github}
+            wrapLines
+            codeBlock
+        />
+    );
+  } else {
+    return (
+        <CopyBlock
+            text={code}
+            language={lang}
+            showLineNumbers={lines > 5}
+            theme={github}
+            wrapLines
+            codeBlock
+        />
+    )
+  }
 };
 
 export default Code
