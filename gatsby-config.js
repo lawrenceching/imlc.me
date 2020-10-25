dc9b70a9c21300e4d0908ee3238719c94632b6b3
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-10-25 10:57:35 +0800

    Set head to true for Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,15 +100,15 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-57304811-3",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: [ ],
+        // exclude: [ ],
         // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
+        // pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Enables Google Optimize Experiment ID
@@ -116,10 +116,10 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Defers execution of google analytics script after page load
-        defer: false,
+        // defer: false,
         // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
         cookieDomain: "www.imlc.me",
       },
     },
