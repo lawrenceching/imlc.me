43d61de1295100f99e23d03d1affefefb4c0f60f
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-11-07 19:27:19 +0800

    Add description meta

--- src/templates/post.js
+++ src/templates/post.js
@@ -96,9 +96,9 @@ class Post extends React.Component {
           <Helmet>
             <meta charSet="utf-8"/>
             <title>{title}{" | IMLC.ME"}</title>
-            <meta description="{description}"/>
-            <meta keywords="Java, JavaScript, Backend, Software Engineer"/>
-            <link rel="canonical" href="http://mysite.com/example"/>
+            <meta name="description" content={description} />
+            <meta name="keywords" content="Java, JavaScript, Backend, Software Engineer" />
+            <link rel="canonical" href="https://www.imlc.me"/>
           </Helmet>
           <Affix style={{ position: 'fixed', bottom: 10, left: 10, zIndex: 1 }}>
             <Avatar />
